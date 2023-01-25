fb12c7375afc83fc2973799ddada28f9e2cf7e76
Mikhail <kameel018rus@gmail.com>
2023-01-25 03:53:15 +0300

    Нахождение элемента массива,равный find

---
+++ Example009/Program.cs
@@ -0,0 +1,24 @@
+﻿//Имеется одномерный массив array из n элементов,
+//требуется найти элемет массива,равный find
+
+
+
+
+
+
+int[] array ={10,22,17,37,42,45,66,17,82};
+
+int n = array.Length;
+int find = 17;
+
+int index = 0;
+
+while( index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
