292bda196aeb4c82cb6b3fde8a9fb157b9e5eea5
Mikhail <kameel018rus@gmail.com>
2023-01-31 00:45:48 +0300

    Простые примеры использования рекурсии

---
+++ Examples015/Program.cs
@@ -0,0 +1,30 @@
+﻿// Простой пример использования рекурсии
+
+/*double Factorial(int n) // Функция нахождения факториала.
+{
+    //1! = 1
+    //0! = 1
+    if (n == 1)
+        return 1;
+    else
+        return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    System.Console.WriteLine($"{i}! = {Factorial(i)}");
+}*/
+
+//Числа Фибоначчи (каждое следующее число задается через предыдущее)
+// f(1)=1
+// f(2)=1
+// f(n)= f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for( int i = 1; i < 40; i++)
+{
+    System.Console.WriteLine(Fibonacci(i));
+}
